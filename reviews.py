a8cf441d6b1281a38997688017d1d93af8549502
Mancy <clare99048@gmail.com>
2023-01-31 11:58:59 +0800

    ver.2_without\n

--- reviews.py
+++ reviews.py
@@ -1,21 +1,17 @@
 data = []
-length = []
+len_sum = 0
 count = 0
 with open('reviews.txt', 'r') as f:
     for line in f:
-        data.append(line.strip())
-        count += 1
-        for  word in line:
-            length.append(word)
-        if count % 10000 == 0:
+        data.append(line)
+        len_sum += len(line.split())
+        if len(data) % 10000 == 0:
             print(len(data))
 print('檔案讀取完畢，共', len(data), '筆留言')
-avg_length = len(length)/len(data)
-print(avg_length)
-# for word in data[0]:
-#     length.append(word.replace(' ', ''))
-# print(data[0])
-# print(len(length))
+
+print('留言平均長度為', len_sum/len(data))
+print(len(data[0].split()))
+
 
     
         
