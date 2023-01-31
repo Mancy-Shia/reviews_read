f1b30ffa77ab63b9dca29647529e8833893d682c
Mancy <clare99048@gmail.com>
2023-01-31 13:25:04 +0800

    filtering_by_word

--- reviews.py
+++ reviews.py
@@ -4,18 +4,18 @@ count = 0
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
-        len_sum += len(line.split())
+        #len_sum += len(line.split())
         # if len(data) % 10000 == 0:
         #     print(len(data))
 print('檔案讀取完畢，共', len(data), '筆留言')
-print('留言平均長度為', len_sum/len(data))
+#print('留言平均長度為', len_sum/len(data))
 
 new =[]
 for d in data:
-    if len(d.split())>200:
+    if 'terrible' in d:
         new.append(d)
-        print(len(d.split()))
-print('共有', len(new), '筆留言大於200')
+print('共有', len(new), '提及terrible')
+print(new[5])
 
 
 
