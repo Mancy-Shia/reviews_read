3468b2de77174a8bd31c5b42871fec74e1162388
Mancy <clare99048@gmail.com>
2023-01-31 13:16:11 +0800

    ver.3_filtering

--- reviews.py
+++ reviews.py
@@ -5,12 +5,18 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         len_sum += len(line.split())
-        if len(data) % 10000 == 0:
-            print(len(data))
+        # if len(data) % 10000 == 0:
+        #     print(len(data))
 print('檔案讀取完畢，共', len(data), '筆留言')
-
 print('留言平均長度為', len_sum/len(data))
-print(len(data[0].split()))
+
+new =[]
+for d in data:
+    if len(d.split())>200:
+        new.append(d)
+        print(len(d.split()))
+print('共有', len(new), '筆留言大於200')
+
 
 
     
